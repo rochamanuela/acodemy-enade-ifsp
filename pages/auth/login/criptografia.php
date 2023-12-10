6feedfe7b47b4536e802ee92fe41bd2415b313c6
rochamanuela <rochadesouzamanuela@gmail.com>
2023-12-10 17:38:52 -0200

    10/12/2023 - 17:38

---
+++ pages/auth/login/criptografia.php
@@ -0,0 +1,8 @@
+<?php
+  function criptografando() {
+    include('conexao.php');
+    $senha = $mysqli->real_escape_string($_POST['senha']);
+    $cripografia = base64_encode($senha);
+    return $cripografia;
+  }
+?>
