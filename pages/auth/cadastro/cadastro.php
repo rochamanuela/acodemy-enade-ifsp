4c1adcf145c6b46526d356e2e753b508a9c7be11
rochamanuela <rochadesouzamanuela@gmail.com>
2023-12-10 18:09:12 -0200

    10/12/2023 - 18:10

--- pages/auth/cadastro/cadastro.php
+++ pages/auth/cadastro/cadastro.php
@@ -11,12 +11,12 @@ if($_POST['senha'] === $_POST['confirmar_senha']){
   $senhas_diferentes = false; // campos são iguais então informa erro
   $senha = criptografando();
   $sql_code = "SELECT * FROM usuarios where email = '$email'";//select email inserido para cadastro no banco de dados 
-  $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código sql: " . $mysqli->erro);
+  $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código sql: " . $mysqli->error);
   $quantidade = $sql_query-> num_rows;
 
   if($quantidade == 0 ){
     $sql_code = "INSERT INTO usuarios(id_usuario, email, senha, nome) VALUES ('','$email','$senha','$nome');";
-    $sql_query = $mysqli ->query($sql_code) or die("Falha na execução do código sql: " . $mysqli->erro);
+    $sql_query = $mysqli ->query($sql_code) or die("Falha na execução do código sql: " . $mysqli->error);
     header("location: ../html/login.html");
   }
 }
